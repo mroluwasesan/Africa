6b32923d5200efe35ae4cf4475485719138062ad
mroluwasesan <mr.oluwasesan@gmail.com>
2023-04-22 10:54:22 +0100

    Designing the detail View

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -21,8 +21,12 @@ struct ContentView: View {
                         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
                     
                     ForEach(animals) { item in
-                        AnimalListItemView(animal: item)
-                    }
+                        NavigationLink(destination: AnimalDetailView(animal: item))
+                        {
+                            AnimalListItemView(animal: item)
+                            
+                        }
+                    }//LINK
                 }//: List
                 .navigationBarTitle("Africa",displayMode: .large)
             }//: NAVIGATION

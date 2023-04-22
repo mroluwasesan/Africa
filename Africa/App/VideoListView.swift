659f4ccd88a6648ebef091ecd519520a96b4a219
mroluwasesan <mr.oluwasesan@gmail.com>
2023-04-22 14:13:14 +0100

    creating a video play list

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct VideoListView: View {
-    var body: some View {
-        Text("Video")
-    }
-}
+  // MARK: - PROPERTIES
+  
+  @State var videos: [Video] = Bundle.main.decode("videos.json")
+  
+  let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+
+  // MARK: - BODY
+
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(videos) { item in
+            VideoListItemView(video: item)
+              .padding(.vertical, 8)
+          }
+        } //: LOOP
+      } //: LIST
+      .listStyle(InsetGroupedListStyle())
+      .navigationBarTitle("Videos", displayMode: .inline)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          Button(action: {
+            // Shuffle videos
+            videos.shuffle()
+            hapticImpact.impactOccurred()
+          }) {
+            Image(systemName: "arrow.2.squarepath")
+          }
+        }
+      }
+    } //: NAVIGATION
+  }
+
+
+// MARK: - PREVIEW
 
 struct VideoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        VideoListView()
-    }
+  static var previews: some View {
+    VideoListView()
+      .previewDevice("iPhone 13")
+  }
 }
---
+++ Model/VideoModel.swift
@@ -0,0 +1,20 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Negatron on 22/04/2023.
+//
+
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+  let id: String
+  let name: String
+  let headline: String
+  
+  // Computed Property
+  var thumbnail: String {
+    "video-\(id)"
+  }
+}

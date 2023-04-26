3e1d626a28f80074ff3fa9ff9ed1111d9e01a692
mroluwasesan <mr.oluwasesan@gmail.com>
2023-04-26 20:37:24 +0100

    mac version with mac Catalyst

---
+++ View/CreditsView.swift
@@ -0,0 +1,36 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Negatron on 26/04/2023.
+//
+import SwiftUI
+
+struct CreditsView: View {
+  var body: some View {
+    VStack {
+      Image("compass")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 128, height: 128)
+      
+      Text("""
+  Copyright © Oluwasesan Rotimi
+  All right reserved
+  Better Apps ♡ Less Code
+  """)
+        .font(.footnote)
+        .multilineTextAlignment(.center)
+    } //: VSTACK
+    .padding()
+    .opacity(0.4)
+  }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+  static var previews: some View {
+    CreditsView()
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
